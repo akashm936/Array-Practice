53d76d7f40a21f7a7bf206ef5c5bdcd9cc41e6cf
Akash Morwal <akashbhau101@gmail.com>
2024-06-16 09:31:21 +0530

    new program is updated ( 16-06-2024)

---
+++ Array9.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+// finding the max element of an array
+
+class Array9
+{
+	public static void main(String[] args) {
+		 
+		 Scanner sc = new Scanner(System.in);
+
+		 System.out.println("Enter a size of array : ");
+		 int size = sc.nextInt();
+
+		 System.out.println("Enter An Element in an array");
+		 int []arr = new int[size];
+
+		 // for get input
+
+		 for(int i=0;i<size;i++)
+		 {
+		 	arr[i]=sc.nextInt();
+		 }
+
+		 // output
+         int max = arr[0]; // intialiy declare 0
+
+		 for(int i=0;i<arr.length;i++)
+		 {
+            if(arr[i]>max)
+            {
+            	max = arr[i];
+            }
+		 }
+		 System.out.println("The Max Element of an Array is  : "+ max);
+	}
+}
+
+
