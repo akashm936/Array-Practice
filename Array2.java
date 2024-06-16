e86f91e77864f54817b3bddb2992da68a79624ea
Akash Morwal <akashbhau101@gmail.com>
2024-06-16 07:37:27 +0530

    new program is updated (16-06-2024)

---
+++ Array2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+// get element from a user
+class Array2
+{
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+       
+        
+		System.out.println("Enter Element to create a Array :");
+		int a = sc.nextInt();
+          
+	// remaining
+        
+		
+
+	}
+}
