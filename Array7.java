242e5f6cf4e5291c0d388ece8daf63e9510ea321
Akash Morwal <akashbhau101@gmail.com>
2024-06-16 09:09:30 +0530

    new program is updated (16-06-2024)

--- Array7.java
+++ Array7.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
 
+// find the min value form the an array
+
 class Array7
 {
 
-	Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 
 	System.out.println("Enter the Size of Array");
 	int size = sc.nextInt();
@@ -16,5 +20,23 @@ class Array7
 	for(int i=0;i<size;i++)
 	{
 		arr[i]= sc.nextInt();
+	}
+    
+    // get min variable
+	int min = arr[0];
+    
+    // used for loop to find min valu form array
+
+    for(int i=0;i<arr.length;i++)
+	{
+		if(arr[i]<min)
+		{   
+			min = arr[i];
+			
+		}
+
+	}
+	System.out.println("minimum value of array : "+ min);
+	
 	}
 }
