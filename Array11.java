8c469fae67f7e8f52d2fe0b2e54269cdd5d2a224
Akash Morwal <akashbhau101@gmail.com>
2024-06-16 19:34:52 +0530

    new program of find element index is updated (16-06-2024)

---
+++ Array11.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Arrray11
+{
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int arr[] = { 9,6,2,5,8,1};
+
+		System.out.println("Enter to find :");
+		int fe = sc.nextInt();
+
+        boolean flag = true;
+
+        outerloop:
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]==fe)
+			{
+				System.out.println(fe + " is present in array at index : "+ i);
+			}
+		} 
+	}
+}
