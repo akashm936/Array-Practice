bdc8fee660ab55adc664689166602f271eccf52a
Akash Morwal <akashbhau101@gmail.com>
2024-06-21 14:34:42 +0530

    new program is updated of string compare (21-06-2024)

---
+++ CompareString.java
@@ -0,0 +1,19 @@
+//what we have to do now?
+
+class CompareString
+{
+	public static void main(String[] args) {
+		
+		String name1 = new String("java");
+		String name2 = new String("java");
+
+        StringBuffer sb1 = new StringBuffer("python");
+        StringBuffer sb2 = new StringBuffer("python");
+
+        int a=4;
+        int b=4;
+		System.out.println(name1==name2);
+	}
+}
+
+// the object are compare 
