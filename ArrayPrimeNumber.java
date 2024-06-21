860f13fba9bb900f37d1843d239a3020f19ce3e9
Akash Morwal <akashbhau101@gmail.com>
2024-06-22 01:13:27 +0530

    new program is updated (22-06-2024)

--- ArrayPrimeNumber.java
+++ ArrayPrimeNumber.java
@@ -11,7 +11,7 @@ class ArrayPrimeNumber
 		int [] arr = {2,3,4,7,13,14,15,16,17};
 		for(int i=0;i<arr.length;i++)
 		{
-				int count = 0;
+			int count = 0;
 				for(int j=0;j<=arr[i];j++)
 				{
 					if(arr[i]%j==0)
@@ -23,44 +23,44 @@ class ArrayPrimeNumber
 				{
 					System.out.println(arr[i]+"");
 				}
-			}
+		}
 
 		}
 
 	}
 
-class ArrayPrime
-{
+// class ArrayPrime
+// {
 
 
 
-	public static void main(String[] args) {
-		{
-			int []a = { 1,2,3,4,,5,6,7,8,9,10};
-			for(int i=0;i<a.length;i++)
-			{
-				boolean flag = true;
-				if(a[i]==1)
-				{
-					flag=false;
-				}
+// 	public static void main(String[] args) {
+// 		{
+// 			int []a = { 1,2,3,4,,5,6,7,8,9,10};
+// 			for(int i=0;i<a.length;i++)
+// 			{
+// 				boolean flag = true;
+// 				if(a[i]==1)
+// 				{
+// 					flag=false;
+// 				}
 
-				for(int j=2;j<a[i];j++)
-				{
-					if(a[i]%j==0)
-					{
-						flag=false;
-						break;
-					}
-				}
-				if(flag)
-				{
-					System.out.println("prime number : "+a[i]);
-				}
-				else{
-					System.out.println("Not a Prime Number : "+ a[i]);
-				}
-			}
-		}
-	}
-}
+// 				for(int j=2;j<a[i];j++)
+// 				{
+// 					if(a[i]%j==0)
+// 					{
+// 						flag=false;
+// 						break;
+// 					}
+// 				}
+// 				if(flag)
+// 				{
+// 					System.out.println("prime number : "+a[i]);
+// 				}
+// 				else{
+// 					System.out.println("Not a Prime Number : "+ a[i]);
+// 				}
+// 			}
+// 		}
+// 	}
+// }
